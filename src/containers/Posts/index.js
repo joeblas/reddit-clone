e37ed1e56d6d8b3b1eddc2caf09858338fd6b411
Joe Gallegos <jgallegos@celticbank.com>
2018-02-28 15:21:39 -0700

    ability to add posts and read posts from firebase

---
+++ src/containers/Posts/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Posts extends Component {
+  render() {
+    let posts = this.props.posts;
+
+    if(this.props.loading) {
+      return (
+        <div>
+          Loading...
+        </div>
+      );
+    }
+
+    return (
+      <div className="Posts">
+        { Object.keys(posts).map( key => {
+          return (
+            <div key={key}>
+              { posts[key].title }
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+export default Posts;
